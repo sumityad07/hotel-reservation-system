1088295b40202d8be51ed01d96ec33f535e346a8
sumityad07 <167276649+sumityad07@users.noreply.github.com>
2025-07-30 17:32:07 +0530

    Update API base URL to deployed Render backend

--- frontend/src/store/axiosInstance.js
+++ frontend/src/store/axiosInstance.js
@@ -2,12 +2,13 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://hotel-reservation-system-pu4l.onrender.com/api',
+  baseURL: 'https://hotel-reservation-system-h3d2.onrender.com/api/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 axiosInstance.interceptors.request.use(
   (config) => {
